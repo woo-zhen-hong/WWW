545d636a24507e89803adb0cd42cdceef00ff20c
MingYao <nunu0831@gmail.com>
2023-04-23 20:23:30 +0800

    no message

--- my-app/src/Debt_Information.js
+++ my-app/src/Debt_Information.js
@@ -53,32 +53,18 @@ const DebtInformation = () => {
             title: '欠款人',
             dataIndex: 'debt_name',
             key: 'debt_name',
-            render: (_, record) => (
-                <>
-                    <a style={{ color: 'black' }}>JACKY</a>
-                </>
-            ),
         },
         {
             title: '還款人',
             dataIndex: 'back_name',
             key: 'back_name',
-            render: (_, record) => (
-                <>
-                    <a style={{ color: 'black' }}>軟妹</a>
-                </>
-                
-            ),
+            
         },
         {
             title: '金額',
             dataIndex: 'money',
             key: 'money',
-            render: (_, record) => (
-                <>
-                    <a style={{ color: 'red' }}>250</a>
-                </>
-            ),
+           
         },
         {
             title: '資訊',
@@ -104,21 +90,13 @@ const DebtInformation = () => {
             title: '日期',
             dataIndex: 'date',
             key: 'date',
-            render: (_, record) => (
-                <>
-                    <a style={{ color: 'black' }}>05/01</a>
-                </>
-            ),
+            
         },
         {
             title: '備註',
             dataIndex: 'note',
             key: 'note',
-            render: (_, record) => (
-                <>
-                    <a style={{ color: 'black' }}>麻辣燙的錢</a>
-                </>
-            ),
+            
         },
         // {
         //     title: '是否還款',
@@ -140,14 +118,15 @@ const DebtInformation = () => {
         },
     ]);
     const [data, setData] = useState([{
-        debt_name: '',
-        back_name: '',
-        money: '',
+        debt_name: 'JACKY',
+        back_name: '軟妹',
+        money: '250',
         tags: '欠款',
-        date: '',
-        note: '',
+        date: '05/01',
+        note: '麻辣燙的錢',
         // back: '',
     },]);
+    
     const addDebt = () => {
         setOpen(true)
         setTitle('新增欠款');
