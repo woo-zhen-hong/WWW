e14684ad202914bdb95c257ef56721464a5787e3
juan <ashley910521@gmail.com>
2023-04-24 20:02:46 +0800

    no message

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path={'/WWW'}>
+        <Route path={'/www'}>
           <Route index element={<Login />} />
           <Route path={'/Login'} element={<Login />} />
           <Route path={'/Register'} element={<Register />} />
