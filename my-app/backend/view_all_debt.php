8b4c75e63e014c29d2a267e2f5ca24d661842e97
MingYao <nunu0831@gmail.com>
2023-06-10 18:08:32 +0800

    全部顯示

--- my-app/backend/view_all_debt.php
+++ my-app/backend/view_all_debt.php
@@ -2,15 +2,20 @@
 session_start(); // 啟用交談期
 // Start the session
 include "index.php";
-$sql = "SELECT  list.amount 'money',list.debt_date 'date',list.debt_alert 'tags' 
+$sql = "SELECT  list.amount 'money',list.debt_date 'date' 
 ,list.debt_note 'note'
-, list.debt_user_id_1 'note', user.name 'debt_name'
-, list.debt_user_id_2 'note', user2.name 'back_name'
+, user.name 'debt_name'
+, user2.name 'back_name'
+,
+case
+when user.id = 1 then 0
+when user2.id = 1 then 1
+END
+AS Answer
 FROM www.list
-LEFT JOIN www.user ON list.debt_user_id_1 = user.id AND user.id 
+LEFT JOIN www.user ON list.debt_user_id_1 = user.id 
 LEFT JOIN www.user user2 ON list.debt_user_id_2 = user2.id
-WHERE user.id = 1 or user2.id = 1 
-     ";
+WHERE user.id = 1 or user2.id = 1 ";
 
 $result = mysqli_query($con, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
