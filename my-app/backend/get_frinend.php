0b6fd7cdcd2cfa2f24293a241d76c8b3fa20dcc0
juan <ashley910521@gmail.com>
2023-06-10 18:11:06 +0800

    拿好友列表

--- my-app/backend/get_frinend.php
+++ my-app/backend/get_frinend.php
@@ -2,9 +2,22 @@
 session_start(); // 啟用交談期
 // Start the session
 include "index.php";
-$use_id = $_SESSION['id'];
-$sql = "
-     ";
+$user_id = $_SESSION['id'];
+$sql = "SELECT * From 
+        (
+            SELECT user.id, user.name 
+            FROM www.user
+            LEFT JOIN www.friend ON friend.friend_id_1=user.id
+            WHERE friend.friend_id_2=$user_id
+        )friend_tmp1
+        UNION 
+        SELECT * From 
+        (
+            SELECT user.id, user.name 
+            FROM www.user
+            LEFT JOIN www.friend ON friend.friend_id_2=user.id
+            WHERE friend.friend_id_1=$user_id
+        )friend_tmp2";
 
 $result = mysqli_query($con, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
