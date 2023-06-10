1f21c2d3302054fa6e353e4b3e6411c8443a4175
juan <ashley910521@gmail.com>
2023-06-10 12:35:00 +0800

    no message

--- my-app/backend/index.php
+++ my-app/backend/index.php
@@ -8,6 +8,11 @@ $hostname = '36.238.120.118';
 $username = 'jk';
 $password = 'jk123';
 $database = 'www';
+// $hostname = '140.127.74.144';
+// $username = '410977004';
+// $password = '410977004';
+// $database = '410977004';
+
 
 // db connection
 $con = mysqli_connect($hostname, $username, $password, $database) or die("Error " . mysqli_error($con));
