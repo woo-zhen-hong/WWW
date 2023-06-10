ac57a68ed4cf615f43e98e4795d4945c8303e367
jackywoo <jackywu0203@gmail.com>
2023-06-10 14:34:24 +0800

    no message

--- my-app/backend/login.php
+++ my-app/backend/login.php
@@ -1,15 +1,19 @@
 <?php
 session_start();  // 啟用交談期
 // Start the session
-include "index.php";
+// include "index.php";
+include "index2.php";
 $request_body = file_get_contents('php://input');
 $data = json_decode($request_body);
 $email = $data->email;
 $password = $data->password;
 if ($email != "" && $password != "") {
-    $sql = "SELECT * FROM  `410977004`.user
-             WHERE `410977004`.user.password = '$password' 
-             AND `410977004`.user.email= '$email' ";
+    // $sql = "SELECT * FROM  `410977004`.user
+    //          WHERE `410977004`.user.password = '$password' 
+    //          AND `410977004`.user.email= '$email' ";
+    $sql = "SELECT * FROM  `www`.user
+                WHERE `www`.user.password = '$password' 
+                AND `www`.user.email= '$email' ";
     // var_dump($sql);
     $stmt = $con->prepare($sql);
     // set parameters and execute
