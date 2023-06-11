dcf7f17ec6417c965fad7594999208edad3ca8f5
MingYao <nunu0831@gmail.com>
2023-06-11 14:36:42 +0800

    三天內

---
+++ my-app/backend/3days_ago.php
@@ -0,0 +1,29 @@
+<?php
+session_start(); // 啟用交談期
+// Start the session
+include "index.php";
+$user_id = $_SESSION['id'];
+$sql = "SELECT  list.id 'list_id',  list.amount 'money',list.debt_date 'date' 
+,list.debt_note 'note'
+, user.name 'debt_name'
+, user2.name 'back_name'
+,case
+when user.id = 1 then 0
+when user2.id = 1 then 1
+END
+AS tag
+FROM www.list
+LEFT JOIN www.user ON list.debt_user_id_1 = user.id 
+LEFT JOIN www.user user2 ON list.debt_user_id_2 = user2.id
+WHERE (user.id = $user_id or user2.id = $user_id)
+AND abs(current_date() - list.debt_date) <=3
+ORDER BY list.debt_date";
+$result = mysqli_query($con, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+}
+$dataset = array(
+    "total" => count($array),
+    "data" => $array
+);
+echo json_encode($dataset);
