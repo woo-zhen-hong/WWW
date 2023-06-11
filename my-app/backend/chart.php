389f826a21f687650972096afa57467a7378b134
juan <ashley910521@gmail.com>
2023-06-11 15:08:06 +0800

    no message

--- my-app/backend/chart.php
+++ my-app/backend/chart.php
@@ -4,22 +4,22 @@ session_start(); // 啟用交談期
 include "index.php";
 $user_id = $_SESSION['id'];
 $sql = "SELECT  list.amount 'money',list.debt_date 'date' 
-    ,list.debt_note 'note'
-   , user.name 'debt_name'
-   , user2.name 'back_name'
-   ,sum(list.amount) 'sum'
-   ,
-case
- when user.id = 1 then 0
- when user2.id = 1 then 1
-END
-AS tag
-FROM www.list
-LEFT JOIN www.user ON list.debt_user_id_1 = user.id 
-LEFT JOIN www.user user2 ON list.debt_user_id_2 = user2.id
-WHERE user2.id = $user_id
-group by MONTH(list.debt_date)
-order by list.debt_date";
+            ,list.debt_note 'note'
+        , user.name 'debt_name'
+        , user2.name 'back_name'
+        ,sum(list.amount) 'sum'
+        ,
+        case
+        when user.id = 1 then 0
+        when user2.id = 1 then 1
+        END
+        AS tag
+        FROM www.list
+        LEFT JOIN www.user ON list.debt_user_id_1 = user.id 
+        LEFT JOIN www.user user2 ON list.debt_user_id_2 = user2.id
+        WHERE user2.id = $user_id
+        group by MONTH(list.debt_date)
+        order by list.debt_date";
 $result = mysqli_query($con, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
     $array[] = $row;
